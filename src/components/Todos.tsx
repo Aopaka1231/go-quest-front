fefa59e94843afa94fb186fb2277260020c2cfdd
Aopaka1231 <hiroyuki_kuroiwa@icloud.com>
2023-07-10 22:10:55 +0900

    add:フロントの実装

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,14 +1,15 @@
 import axios from "axios";
+import React from "react";
 import { useEffect, useState } from "react";
 
 interface Todo {
-    id: number;
-    content: string;
+    id: number,
+    content: string,
 }
 
 export const Todos = () => {
-    const [inputValue, outputValue] = useState<string>("");
-    const [todos, setTodos] = useState<Todo[]>("");
+    const [inputValue, setInputValue] = useState<string>("");
+    const [todos, setTodos] = useState<Todo[]>([]);
 
     useEffect(() => {
         const FetchData = async () => {
@@ -23,5 +24,60 @@ export const Todos = () => {
                 });
         };
         FetchData();
-    }, []);
-};
+    }, [])
+
+    const handleSubmit = async() => {
+        console.log(inputValue)
+        await axios.post('http://8000/todo'.{
+            "content": inputValue
+        })
+        .then(res => res.data)
+        .then(data => {
+            console.log('Success:', data);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+
+        setInputValue("")
+
+        await axios.get('http://localhost:8000/todo')
+        .then(res => res.data)
+        .then(data => {setTodos(data)})
+        .catch(error => {
+            console.error("Error:",error)
+        })
+    }
+
+    const handleDelete = async(id:number) => {
+        await axios.delete(`http://localhost:8000/todo/${id}`)
+        .then(res => res.data)
+        .then(data => {
+            console.log(data)
+        })
+        .catch(error => {
+            console.error(error)
+        })
+    }
+
+    return (
+        <div className="form-wrapper">
+            <div>
+                <input value={inputValue} type="text" id="content" onChange={(e)=>{setInputValue(e.target.value)}}/>
+                <button onClick={handleSubmit}>追加</button>
+            </div>
+            <ul className="todos">
+                {
+                    todos.map((todo) => {
+                        return(
+                            <li key={todo.id}>
+                                {todo.content}
+                                <button onClick={()=>{handleDelete(todo.id)}}>削除</button>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        </div>
+    )
+}
